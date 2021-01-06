91b0b755c81a6fbc50c7e29f0df65afea596aa35
bookgvi <bookgvi@gmail.com>
2021-01-06 17:46:04 +0300

    иной способ заполнения массива forks

--- src/com/philosophers/dining/Table.java
+++ src/com/philosophers/dining/Table.java
@@ -1,17 +1,12 @@
 package com.philosophers.dining;
 
-import java.util.*;
 import java.util.stream.IntStream;
 
 public class Table {
   public static void main(String[] args) {
     final int PHILOSOPHERS_COUNT = 5;
     Philosopher[] philosophers = new Philosopher[PHILOSOPHERS_COUNT];
-    Object[] forks = new Object[PHILOSOPHERS_COUNT];
-
-    for (int i = 0; i < forks.length; i++) {
-      forks[i] = new Object();
-    }
+    Object[] forks = IntStream.range(0, PHILOSOPHERS_COUNT).mapToObj(i -> new Object()).toArray();
 
     for(int i = 0; i < philosophers.length; i++) {
       Object rightFork = forks[(i + 1) % forks.length];
