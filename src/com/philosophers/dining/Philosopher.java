9f48b02ea27fc256f63b705a76ee836bd1296ebf
bookgvi <bookgvi@gmail.com>
2021-01-06 16:29:40 +0300

    Philosopher runnable class

---
+++ src/com/philosophers/dining/Philosopher.java
@@ -0,0 +1,37 @@
+package com.philosophers.dining;
+
+import java.util.Random;
+
+public class Philosopher implements Runnable {
+  private final Object leftFork;
+  private final Object rightFork;
+
+  Philosopher(Object leftFork, Object rightFork) {
+    this.leftFork = leftFork;
+    this.rightFork = rightFork;
+  }
+
+  @Override
+  public void run() {
+    try {
+      while (true) {
+        doAction(" thinking...");
+        synchronized (leftFork) {
+          doAction("pick up left fork");
+          synchronized (rightFork) {
+            doAction("pick up right fork");
+            doAction("put right fork");
+          }
+          doAction("put left fork");
+        }
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void doAction(String action) throws InterruptedException {
+    System.out.printf("%s %s%n", Thread.currentThread().getName(), action);
+    Thread.sleep(new Random().nextInt(500));
+  }
+}
